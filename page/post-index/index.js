aca68149b0385f1522b30030c539906e089656a3
liulingling <liulingling@astroway.net>
2018-02-27 17:20:14 +0800

    完善了兼职模块的代码

--- page/post-index/index.js
+++ page/post-index/index.js
@@ -8,8 +8,8 @@ P('index', {
 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
       ],
-      indicatorDots: false,
-      autoplay: false,
+      interval: false,
+      autoplay: true,
       interval: 5000,
       duration: 1000,
 
