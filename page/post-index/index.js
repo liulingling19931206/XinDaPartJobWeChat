ddb8ac5d59952b94094be189ffa0326be904d8ff
liulingling <liulingling@astroway.net>
2018-02-28 19:20:13 +0800

    兼职首页页面样式的搭建

--- page/post-index/index.js
+++ page/post-index/index.js
@@ -4,9 +4,9 @@ P('index', {
     data: {
       //1：轮播图相关配置
       imgUrls: [
-'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+'../../image/post/banner1.png',
+'../../image/post/banner1.png',
+'../../image/post/banner1.png'
       ],
       indicatorDots: false,
       autoplay: true,
