c82c58a6e78e9e352f4df051869f99fde5b5c5b6
liulingling <liulingling@astroway.net>
2018-03-01 11:40:36 +0800

    搭建了兼职详情页面的实现

---
+++ page/part-time-detail/index.js
@@ -0,0 +1,37 @@
+/************付费课堂页面****************/
+var P = require('../../lib/wxpage')
+P('index', {
+    data: {
+    },
+
+    onLaunch: function () {
+    },
+
+    /**
+     * 初次加载页面配置
+     * **/
+    onLoad: function (options) {
+      wx.setNavigationBarColor({
+        frontColor: '#ffffff',
+        backgroundColor: '#37383b',
+        animation: {
+          duration: 400,
+          timingFunc: 'easeIn'
+        }
+      })
+      wx.setNavigationBarTitle({
+        title: '兼职详情'
+      })
+      wx.showShareMenu({
+        withShareTicket: true
+      })
+    },
+
+    /**
+       * 下拉刷新
+       * **/
+    onPullDownRefresh: function () {
+      //this.getPayCourseList();
+      wx.stopPullDownRefresh()
+    },
+})
